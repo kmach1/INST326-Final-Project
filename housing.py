103e8d32b7fe641494b608f06e0496d7e8377c16
Sbamba1023 <149190097+Sbamba1023@users.noreply.github.com>
2024-11-12 14:30:02 -0500

    Explaining project

--- housing.py
+++ housing.py
@@ -1,2 +1,9 @@
+""" Final Project: Student Housing FInder
+UMD Student Housing Finder Framework
+
+A Python framework designed to help University of Maryland students find suitable housing 
+by filtering and comparing rental properties based on customizable criteria.
+
+"""
 class Housing:
     def __init__(self, id, name, bed, bath, price, sqft, proximity):

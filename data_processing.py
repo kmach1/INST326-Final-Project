0271eea4545558aab5de2f11795ce209f333e591
OutofSpacee <136359989+OutofSpacee@users.noreply.github.com>
2024-11-12 15:12:56 -0500

    Adding doc strings for the InputProcessing
    This add the doc stings for the InputProcessing class within the module data_processing.py to understand the uses for the class.

--- data_processing.py
+++ data_processing.py
@@ -3,6 +3,18 @@ Resposnsible for cleaning and approving the .txt input data
 """
 
 class InputProcessing:
+    """
+    This class is responsible for reading the input data, cleaning it and processing it
+
+    Attributes:
+    input_file (str): The path to the input file
+    
+    Methods:
+    read_input: Read the input file and return the data as a pandas dataframe
+    clean_data: Clean the data and return the cleaned data
+    process_input: Process the input data
+    """
+
     def __init__(self, input_file):
         pass
 

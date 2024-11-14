9afd92ad1aa44e567d67c2a0fcb65bb235a12b91
gkoduol <gkoduol@terpmail.umd.edu>
2024-11-14 16:34:56 -0500

    Added docstring to housing.py

--- housing.py
+++ housing.py
@@ -6,7 +6,16 @@ by filtering and comparing rental properties based on customizable criteria.
 
 """
 class Housing:
-    """include docstring here
+    """  A class representing a housing option for University of Maryland students.
+
+    Attributes:
+        id (int): Unique identifier for the property.
+        name (str): Name or title of the property.
+        bed (int): Number of bedrooms in the property.
+        bath (int): Number of bathrooms in the property.
+        price (float): Monthly rental price of the property.
+        sqft (int): Square footage of the property.
+        proximity (float): Distance from campus in miles or kilometers.
     """
     def __init__(self, id, name, bed, bath, price, sqft, proximity):
         pass
